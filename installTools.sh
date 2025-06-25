8e874678b1cc546bb70584c3b71603831ad9a373
Erget Kabaj (LHIND) <erget.kabaj@lhind.dlh.de>
2025-06-25 15:36:23 +0200

    crating script for cli and terraform and helm

--- installTools.sh
+++ installTools.sh
@@ -18,3 +18,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashi
 
 sudo apt update
 sudo apt-get install terraform
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
